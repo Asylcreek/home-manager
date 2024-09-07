fc361550b65ad3a96f46d17ca09954e06490012e
Omokugbo Joseph Boro <omokugbobr@gmail.com>
2024-09-07 14:25:50 +0100

    chore: Rename packages/ to shell/

--- home.nix
+++ home.nix
@@ -2,7 +2,7 @@
 
 {
   imports = [
-    ./packages
+    ./shell
   ];
   
   # Home Manager needs a bit of information about you and the paths it should
@@ -38,12 +38,6 @@
     go
     redis
     fnm
-
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
 
     # # You can also create simple shell scripts directly inside your
     # # configuration. For example, this adds a command 'my-hello' to your
