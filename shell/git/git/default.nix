5c6dcd5a88454a9e034a9276294369f4d547bf22
Omokugbo Joseph Boro <omokugbobr@gmail.com>
2026-05-28 10:08:09 +0100

    refactor: Add lazygit config

--- shell/git/git/default.nix
+++ shell/git/git/default.nix
@@ -63,17 +63,4 @@
       };
    };
   };
-
-  programs.lazygit = {
-    enable = true;
-
-    settings = {
-      git.paging = {
-        colorArg = "always";
-        pager = "delta --dark --paging=never --line-numbers --hyperlinks --hyperlinks-file-link-format=lazygit-edit://{path}:{line}";
-      };
-
-      os.editPreset = "nvim";
-    };
-  };
 }
